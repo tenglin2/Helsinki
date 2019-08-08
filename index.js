cc91c37801b7dd7d4811fdf8c9c6a6b04442d8a7
Teng Lin <tenglin2@gmail.com>
2019-08-08 10:37:46 -0400

    testing credentials

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(cors());
